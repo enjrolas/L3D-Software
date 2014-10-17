01ffd8a824ca4f9713c22c048e2ca3645ed86407
Alex Hornstein <japhy@tests-MacBook-Pro.local>
2014-10-17 16:15:19 -0400

    updated the listener

--- Streaming/Listener/Listener.ino
+++ Streaming/Listener/Listener.ino
@@ -1,4 +1,3 @@
-#include "colors.h"
 #include "neopixel/neopixel.h"
 
 UDP Udp;
@@ -13,25 +12,77 @@ char data[512];
 #define SIDE 8
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
+char localIP[24];
+char macAddress[20];
+int port;
+int lastUpdated=0;
 
+typedef struct{
+    unsigned char red, green, blue;
+} color;
+
+void setPixel(int x, int y, int z, color col);  //sets a pixel at position (x,y,z) to the col parameter's color
+int setPort(String _port);
 
 void setup() 
 {
+    port=2000;
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
-  Udp.begin (6000);
+  Udp.begin (port);
   Serial.begin(115200);
   Serial.println("initializing...");
+  updateNetworkInfo();
+  initSparkVariables();
 }
+
+//initializes the shared variables and functions that are accessible through the spark API
+//this makes the core's local IP address, MAC address and port accessible to streaming programs
+//that have the access token for the core owner's account
+//The function setPort lets a streaming program set the port on which the core will listen for streaming packets
+void initSparkVariables()
+{
+    Spark.variable("IPAddress", localIP, STRING);
+    Spark.variable("MACAddress", macAddress, STRING);
+    Spark.variable("port", &port, INT);
+    Spark.function("setPort", setPort);
+}
+
+//updates the local IP address and mac address and stores them in global strings
+//if those strings are Spark variables (using the Spark.variable() function), they'll be accessible to the wider world
+//This is all kinds of helpful anytime you're working on a project that uses network communicqtion (streaming, etc)
+
+void updateNetworkInfo()
+{
+    IPAddress myIp = WiFi.localIP();
+    sprintf(localIP, "%d.%d.%d.%d", myIp[0], myIp[1], myIp[2], myIp[3]);
+    byte macAddr[6];
+    WiFi.macAddress(macAddr);
+    sprintf(macAddress, "%02x:%02x:%02x:%02x:%02x:%02x",macAddr[5],macAddr[4],macAddr[3],macAddr[2],macAddr[1],macAddr[0]);
+
+    //print it out the serial port
+    Serial.print("local IP Address: ");
+    Serial.println(localIP);
+    Serial.print("MAC address:  ");
+    Serial.println(macAddress);
+}
+
+int setPort(String _port)
+{
+    port=_port.toInt();
+    Udp.begin(port);
+    return port;
+}
+
 
 void loop (void)
 {
-
-int32_t retries = 0;
 int32_t bytesrecv = Udp.parsePacket();
-while(bytesrecv == 0 && retries < 1000) {
-    bytesrecv = Udp.parsePacket();
-    retries++;
+
+if(millis()-lastUpdated>60000)  //update the network settings every minute
+{
+    updateNetworkInfo();
+    lastUpdated=millis();
 }
 
 if (bytesrecv==PIXEL_COUNT) {
@@ -41,17 +92,9 @@ if (bytesrecv==PIXEL_COUNT) {
             for(int z=0;z<SIDE;z++)
             {
                 int index = z*64 + y*8 + x;
-                if(data[index]!=0)
-                {
-                    Serial.print("position:  (");
-                    Serial.print(x);
-                    Serial.print(", ");
-                    Serial.print(y);
-                    Serial.print(", ");
-                    Serial.print(z);
-                    Serial.println(")");
-                }
-                color pixelColor={ data[index]&0xE0, (data[index]&0x1C)<<3, (data[index]&0x03)<<6}; 
+//                color pixelColor={ data[index]&0xE0, (data[index]&0x1C)<<3, (data[index]&0x03)<<6};   //colors as encoded
+//                color pixelColor={ (data[index]&0xE0)>>1, (data[index]&0x1C)<<2, (data[index]&0x03)<<5};   //colors with max brightness set to 128
+                color pixelColor={ (data[index]&0xE0)>>2, (data[index]&0x1C)<<1, (data[index]&0x03)<<4};   //colors with max brightness set to 64
                 setPixel(x,y,z,pixelColor);
             }
         
@@ -65,3 +108,4 @@ void setPixel(int x, int y, int z, color col)
     int index = z*64 + x*8 + y;
     strip.setPixelColor(index,strip.Color(col.red,  col.green, col.blue));
 }
+
